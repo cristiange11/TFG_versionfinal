96c8b0008805a3502f181642530d41f6ffc49f01
cristiange11 <lolito_311265@hotmail.com>
2021-08-06 12:52:59 +0200

    Añado middleware helmet

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,7 @@ const fileupload = require('express-fileupload');
 const app = express();
 const cors = require('cors');
 const busboy = require("connect-busboy");
+const helmet = require("helmet");
 const ports = process.env.PORT || 3000;
 const authRoutes = require('./routes/auth');
 const centroRoutes = require('./routes/centro');
@@ -22,11 +23,10 @@ const resultadoRoutes= require('./routes/resultadoEncuesta');
 
 app.use(cors());
 
-app.use(fileupload());
-
-app.use(bodyParser.json());
-
-app.use(busboy());
+app.use(express.json());
+app.use(express.json({ limit: '100kb' }))
+app.use(express.json({ parameterLimit: '1000' }))
+app.use(helmet());
 
 app.use('/auth', authRoutes);
 
